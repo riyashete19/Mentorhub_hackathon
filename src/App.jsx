4195de2eeab1530fd51275e852bcccc0a9ef135b
Kalyani <kalyanibonde05@gmail.com>
2024-04-03 00:37:48 +0530

    mentor login and signup

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import React, { useState } from 'react';
 import './App.css';
 import Login from './component/menties/login';
 import Signup from './component/menties/signup';
+import msignup from './component/mentors/mentorsignup';
+import mlogin from './component/mentors/mentorlogin';
 
 function App() {
   return (
     <>
       <Login />
       <Signup/>
+      
     </>
   );
 }
